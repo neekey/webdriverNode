3985b16cc1f17e1c4aba30613dfa692e2b63f698
neekey <ni184775761@gmail.com>
2012-03-29 15:15:13 +0800

    添加Icallback是否文成检查和依赖检查，但是还是有问题

--- examples/example.submit.js
+++ examples/example.submit.js
@@ -4,11 +4,27 @@ var util = require( 'util' );
 
 client
     .init()
-    .url("http://www.google.com", function(){ this.customLog( 'hello! url' );})
-    .setValue("#lst-ib", "webdriver")
-    .submitForm("#tsf", function(){ this.customLog( 'hello! submitForm' );})
-	.pause( 5000 )
-    .end();
+    .url("http://www.google.com", function(){
+        this.customLog( 'hello! url' );
+        this.getTitle(function (){
+
+            this.setValue("#lst-ib", "webdriver")
+                .submitForm("#tsf", function(){
+                    this.customLog( 'hello! submitForm' );})
+
+        }) ;
+    })
+    .pause( 5000, function (){
+
+        this.end(function ( r ){
+
+            console.log( JSON.stringify( r ) );
+        });
+    });
+
+
+
+
 
 
 
