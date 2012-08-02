9c4281200f145d2f0f3291871c1ac73a00cf568f
neekey <ni184775761@gmail.com>
2012-08-02 17:14:07 +0800

    添加test目录以及静态服务器

---
+++ test/serverConfig.js
@@ -0,0 +1,7 @@
+/**
+ * 配置用于测试页面的静态服务器的相关信息
+ */
+module.exports = {
+    port: 8080,
+    host: 'localhost'
+};
---
+++ test/testServer.js
@@ -0,0 +1,17 @@
+/**
+ * 用于测试的静态服务器
+ */
+var static = require('node-static');
+var serverConfig = require( './serverConfig' );
+
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server;
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        // Serve files!
+        file.serve(request, response);
+    });
+}).listen( serverConfig.port );
+console.log( 'test server is now listen port ' + serverConfig.port );
