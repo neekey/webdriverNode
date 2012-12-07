4c1ffefcd181a9990d783923048de1cceb811cd9
neekey <ni184775761@gmail.com>
2012-12-07 11:12:50 +0800

    修复session协议的get请求出错的问题

--- test/cases/protocol.session.spec.js
+++ test/cases/protocol.session.spec.js
@@ -1,7 +1,7 @@
 var assert = require( 'assert' );
 var client = require( '../../lib/core/webdriverNode').remote({
     desiredCapabilities: {
-        browserName:"firefox"
+        browserName:"chrome"
     }
 });
 
@@ -22,15 +22,48 @@ describe( 'Protocol methods', function(){
         });
 
         // 服务器会出错...还是有问题啊...
-        it.skip( 'GET: Get the session info', function( done ){
+        it( 'GET: Get the session info', function( done ){
 
             var sessionId = client._data[ 'sessionId' ];
 
             client.protocol.session('GET', function( ret ){
 
+                /**
+                 * 下面是一个正常返回的对象结构
+
+                var expectRet = {
+                    sessionId: '1354848935413',
+                    status: 0,
+                    value:
+                    {
+                        platform: 'MAC',
+                        'chrome.chromedriverVersion': '23.0.1240.0',
+                        acceptSslCerts: false,
+                        javascriptEnabled: true,
+                        browserName: 'chrome',
+                        rotatable: false,
+                        locationContextEnabled: false,
+                        'webdriver.remote.sessionid': '1354848935413',
+                        version: '23.0.1271.95',
+                        cssSelectorsEnabled: true,
+                        databaseEnabled: false,
+                        handlesAlerts: true,
+                        browserConnectionEnabled: false,
+                        webStorageEnabled: true,
+                        nativeEvents: true,
+                        'chrome.nativeEvents': false,
+                        applicationCacheEnabled: false,
+                        takesScreenshot: true
+                    },
+                    class: 'org.openqa.selenium.remote.Response',
+                    hCode: 427145209
+                };
+
+                */
+
                 assert.equal( 'number', typeof parseInt( sessionId ) );
                 assert.equal( 0, ret.status );
-                assert.equal( sessionId, ret.value );
+                assert.equal( sessionId, Number( ret.sessionId ) );
                 done();
             });
         });
@@ -38,7 +71,6 @@ describe( 'Protocol methods', function(){
         it( 'DELETE: delete a session.', function(done){
 
             client.protocol.session( 'DELETE', function( ret ){
-
                 assert.equal( 0, ret.status );
                 assert.equal( '', ret.value );
                 done();
